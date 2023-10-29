6d0c9889190751e7cc65d0c9506b19a7a952daac
Robert Andrew Moore <ra.moore573@gmail.com>
2023-10-29 15:57:05 -0500

    add Subjects and reworked addIngredients function

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,44 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Pancakes', 'Tasty!', 'https://www.allrecipes.com/thmb/Pn6CJYfILCBbnJgkkPfLWh-Aga0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/45396-easy-pancakes-mfs-38-34bd7f71161446bfb69609fa46a301e4.jpg'),
-    new Recipe('Waffles', 'Tasty!', 'https://stordfkenticomedia.blob.core.windows.net/df-us/rms/media/recipemediafiles/recipes/retail/x17/2020_belgian-style-waffles_16700_600x600.jpg?ext=.jpg')
+    new Recipe('Pancakes',
+      'Tasty!',
+      'https://www.allrecipes.com/thmb/Pn6CJYfILCBbnJgkkPfLWh-Aga0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/45396-easy-pancakes-mfs-38-34bd7f71161446bfb69609fa46a301e4.jpg',
+      [
+        new Ingredient('Flour', 1),
+        new Ingredient('Milk', 1),
+        new Ingredient('Eggs', 2),
+        new Ingredient('Syrup', 1),
+      ]),
+
+    new Recipe('Waffles',
+      'Tasty!',
+      'https://stordfkenticomedia.blob.core.windows.net/df-us/rms/media/recipemediafiles/recipes/retail/x17/2020_belgian-style-waffles_16700_600x600.jpg?ext=.jpg',
+      [
+        new Ingredient('Flour', 1),
+        new Ingredient('Vanilla Extract', 1),
+        new Ingredient('Syrup', 1),
+        new Ingredient('Eggs', 3),
+      ])
   ];
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipeById(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
+
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Flour', 5),
     new Ingredient('Eggs', 3),
@@ -14,8 +15,20 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
-
+  addIngredients(ingredients: Ingredient[]) {
+    for (const ingredient of ingredients) {
+      const ingExists = this.ingredients.find(
+        (ing) => ing.name === ingredient.name
+      );
+      if (ingExists) {
+        ingExists.amount += ingredient.amount;
+      } else {
+        this.ingredients.push(ingredient);
+      }
+    }
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
